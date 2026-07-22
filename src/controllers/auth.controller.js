8537ad11deee85c575436bdf6a8ca879a59026f3
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-07-22 23:29:54 +0530

    "auth controller"

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,39 @@
+import bcrypt from "bcryptjs"
 
 
+export const signup =  async(req ,res)=>{
+     const {fullName , email , password} =  req.body;
 
-export const signup =  async()=>{
+     try {
+       if(!fullName || !email || !password) {
+          return res.status(400).json({Message:"All fields are required"});
+       }
 
+       if(password.length < 6){
+        return res.status(400).json({Message:"Password must be atleast 6 characters long"});
+       } 
+
+       const user = await UserActivation.findOne({email});
+
+       if(user){
+          return res.status(400).json({Message:"User Already exists with this email"});
+       }
+        const salt = await bcrypt.genSalt(10);
+
+        const
+
+     } catch (error) {
+      
+     }
 }
 
-export const login =  async()=>{
+export const login =  async(req , res)=>{
   
 }
-export const logout =  async()=>{
+export const logout =  async(req , res)=>{
   
 }
-export const updateProfile =  async()=>{
+export const updateProfile =  async(req , res)=>{
   
 }
 
