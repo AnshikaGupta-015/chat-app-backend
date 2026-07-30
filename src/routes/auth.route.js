7069067398879d2df4e2890d3779f77e78f802d9
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-07-30 22:34:15 +0530

    "auth routes"

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewaares/uth.middleware.js";
+import checkAuth from "../controllers/auth.controller.js"
 
 const router = express.Router()
 
 router.post("/signup" , signup);
 router.post("/login" , login);
 router.post("/logout" , logout);
+router.get("/check", protectRoute, checkAuth)
 
 router.put("/update-profile" , protectRoute , updateProfile);
 
